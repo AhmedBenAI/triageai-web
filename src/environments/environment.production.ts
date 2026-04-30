c035eb904ccf3d636ffea2dd27c35270af6aeedf
PotatoUser69 <totti312002@gmail.com>
2026-04-30 22:52:56 +0100

    added prod url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '', // replaced at build time by replace-api-url.js when API_URL env var is set
+  apiUrl: 'https://triageai-backend-production.up.railway.app', // replaced at build time by replace-api-url.js when API_URL env var is set
 };
