bcce02ba55da7a50c5401eb5ffc1234558371c5e
PotatoUser69 <totti312002@gmail.com>
2026-04-30 23:12:30 +0100

    name issue

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://triageai-backend-production.up.railway.app', // replaced at build time by replace-api-url.js when API_URL env var is set
+  apiUrl: 'https://triageai-backend-production.up.railway.app',
 };
